2644badbf4b4adc5af44979fcec99a5742698af1
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 18:19:52 +0300

    voting

---
+++ vote.py
@@ -0,0 +1,10 @@
+
+miaka=int(input("Please Enter Your Age:"))
+if miaka>=18:
+    print("Your are eligible to vote")
+else:
+    print("Pole, Haujafikisha miaka za kupiga kura")
+
+#ifx%==00
+phone number=int(input("please enter your phone number"))
+if phone number>=
