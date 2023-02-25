6ac059051a4bc008c5067fab51b5f3f2104b18ba
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 18:08:55 +0300

    loops

---
+++ loops.py
@@ -0,0 +1,15 @@
+
+#for loop
+fruits=["apples," "banana," "oranges"]
+
+for num in range(5, 15):
+    print(num)
+for matunda in fruits:
+    print(matunda)
+
+    print(mynumber)
+#while loop
+num1 = 5
+while num1 <= 15:
+    print(num1)
+    num1 = num1 + 1
