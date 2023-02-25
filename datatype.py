aa35e5c862fbefa82f744e77786a2362435ffe51
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 17:58:54 +0300

    datatypes

---
+++ datatype.py
@@ -0,0 +1,15 @@
+
+jina="Emobilis"
+numbari=23
+nambari1=34.89
+ukweli=True
+JINA="PYTHON CLASS"
+x=float(10)
+
+print(type(jina))
+print(type(ukweli))
+print(type(numbari))
+print(x)
+
+x=float(34)
+print(x)
