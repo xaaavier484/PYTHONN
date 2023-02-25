de342fde782bcb24d0c99f7deaf0e639d88cef8e
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 17:56:01 +0300

    datastructures

---
+++ datastructure.py
@@ -0,0 +1,19 @@
+
+#this is a list
+myclassmate=["Xavier","Jhon","Purity","Emily","Casey","Ann"]
+print(myclassmate[2])
+myclassmate[0]="Ann"
+myclassmate.append("Michael")
+myclassmate.insert(1,"Max")
+print(myclassmate)
+#my tuple
+myfavfruits=("mangoes", "pineapples", "orange")
+
+#set datastructure
+myfavcars={"Toyota", "Mercedes," ,"Nissan"}
+myfavcars.add("Rangerover")
+print(myfavcars)
+#dictionaries
+magari={
+
+}
