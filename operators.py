a2122d79557d1c46ff3a124ea7e46dbcbaf39e32
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 17:54:05 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,24 @@
+
+num1=89
+num2=67
+
+#arithmetic operators
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+print(num1%num2)
+
+#comparison operator
+x=10
+y=15
+z="10"
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<=y)
+print(y>=y)
+
+#logical operator
+print(x<3 and x==10)
+print(x<3 or x==10)
