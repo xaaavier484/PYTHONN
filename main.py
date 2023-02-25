d4ed6ebde5cf315aedf481d1bee36170a9be9a63
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 18:12:06 +0300

    main!

---
+++ main.py
@@ -0,0 +1,61 @@
+
+x=float(34)
+print(x)
+
+jina="Emobilis"
+jina="xavier"
+numbari=23
+nambari1=34.89
+ukweli=True
+JINA="PYTHON CLASS"
+x=float(10)
+
+print(type(jina))
+print
+print(type(ukweli))
+print(type(numbari))
+print(x)
+
+x=float(34)
+print(x)
+
+player_1 = "Mario"
+
+status = "Available"
+status = "busy"
+print(status)
+field = "nano" + "tech"
+print(field + "trading")
+print("99" + "red ballons")
+movie = "Ghostbusters"
+movie = "21" + "Jump" + "Street"
+print(movie)
+average_grade = "B"
+average_grade = "A-"
+subject = "Programming"
+print(average_grade)
+
+is_close = True
+is_far = not is_close
+print(is_far)
+
+is_monday = not True
+print(is_monday)
+
+logged_in = False
+print("logged in:")
+print(logged_in)
+logged_in =not False
+print("logged in:")
+print(logged_in)
+
+is_connected =not False
+
+split =15/5
+print(split)
+print("Even split")
+print(True)
+
+print(235 < 1)
+print(10 < 1)
+print(1 > 1)
