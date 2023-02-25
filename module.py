d777917d1791deb8b7453c0c7dcbc0f843c02ba4
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-02-25 18:13:03 +0300

    modules

---
+++ module.py
@@ -0,0 +1,13 @@
+
+import mymodule
+import math
+import statistics
+import datetime
+mymodule.hello("Ali")
+mymodule.hello("Xavier")
+print(math.sqrt(16))
+dataset=(2,4,3,6,5,7)
+x=statistics.mean(dataset)
+print(the Mean is ", x)
+masaa=datetime.datetime.now()
+print(masaa)
